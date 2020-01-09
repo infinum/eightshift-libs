bfbaa16b74b2fa3584a136fb019f761229978530
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-01-09 12:25:47 +0100

    load time optimisations in manifest and blocks

--- src/manifest/class-manifest.php
+++ src/manifest/class-manifest.php
@@ -36,6 +36,15 @@ class Manifest implements Service, Manifest_Data {
   protected $config;
 
   /**
+   * Full data of manifest items.
+   *
+   * @var array
+   *
+   * @since 2.0.0
+   */
+  protected $manifest = [];
+
+  /**
    * Create a new instance that injects config data to get project specific details.
    *
    * @param Config_Data $config Inject config which holds data regarding project details.
@@ -45,15 +54,6 @@ class Manifest implements Service, Manifest_Data {
   public function __construct( Config_Data $config ) {
       $this->config = $config;
   }
-
-  /**
-   * Transient cache name constant.
-   *
-   * @var string
-   *
-   * @since 2.0.0
-   */
-  const CACHE_NAME = 'manifest-data';
 
   /**
    * Manifest item filter name constant.
@@ -75,7 +75,38 @@ class Manifest implements Service, Manifest_Data {
    * @since 0.6.0 Init.
    */
   public function register() {
+    add_action( 'init', [ $this, 'get_assets_manifest_raw' ] );
     add_filter( $this->config->get_config( static::MANIFEST_ITEM_FILTER_NAME ), [ $this, 'get_assets_manifest_item' ] );
+  }
+
+  /**
+   * Return full manifest data with site url prefix.
+   * You should never call this method directly insted you should call $this->manifest.
+   *
+   * @throws Exception\Invalid_Manifest Throws error if manifest.json file is missing.
+   *
+   * @return array
+   *
+   * @since 2.0.0
+   */
+  public function get_assets_manifest_raw() {
+    $path = $this->config->get_project_path() . '/public/manifest.json';
+
+    if ( ! file_exists( $path ) ) {
+      throw Invalid_Manifest::missing_manifest_exception( $path );
+    }
+
+    $data = json_decode( implode( ' ', file( $path ) ), true );
+    if ( empty( $data ) ) {
+      return [];
+    }
+
+    $this->manifest = array_map(
+      function( $manifest_item ) {
+        return "{$this->get_assets_manifest_output_prefix()}{$manifest_item}";
+      },
+      $data
+    );
   }
 
   /**
@@ -92,67 +123,13 @@ class Manifest implements Service, Manifest_Data {
    * @since 0.6.0 Init
    */
   public function get_assets_manifest_item( string $key ) : string {
-    $data = $this->get_assets_manifest_data();
+    $manifest = $this->manifest;
 
-    if ( ! isset( $data[ $key ] ) ) {
+    if ( ! isset( $manifest[ $key ] ) ) {
       throw Invalid_Manifest::missing_manifest_item_exception( $key );
     }
 
-    return $data[ $key ];
-  }
-
-  /**
-   * Return full manifest data with site url prefix.
-   *
-   * @throws Exception\Invalid_Manifest Throws error if manifest.json file is missing.
-   *
-   * @return array
-   *
-   * @since 2.0.0
-   */
-  protected function get_assets_manifest_raw() : array {
-    $path = $this->config->get_project_path() . '/public/manifest.json';
-
-    if ( ! file_exists( $path ) ) {
-      throw Invalid_Manifest::missing_manifest_exception( $path );
-    }
-
-    $data = json_decode( implode( ' ', file( $path ) ), true );
-    if ( empty( $data ) ) {
-      return [];
-    }
-
-    return array_map(
-      function( $manifest_item ) {
-        return "{$this->get_assets_manifest_output_prefix()}{$manifest_item}";
-      },
-      $data
-    );
-  }
-
-  /**
-   * Get Assets Manifest data depending on the env.
-   * If env is develop output only raw data.
-   * If env is staging or production output cached data in transient.
-   *
-   * @return array
-   *
-   * @since 2.0.0
-   */
-  protected function get_assets_manifest_data() : array {
-    if ( $this->config->get_project_env() === 'develop' ) {
-      return $this->get_assets_manifest_raw();
-    }
-
-    $cache_name = $this->config->get_config( static::CACHE_NAME );
-    $data       = \get_transient( $cache_name );
-
-    if ( $data === false ) {
-      $data = $this->get_assets_manifest_raw();
-      \set_transient( $cache_name, $data );
-    }
-
-    return $data;
+    return $manifest[ $key ];
   }
 
   /**
