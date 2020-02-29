ce2c39b0e80e9d4431f4d53ad320e1e80d563877
Ivan Grginov <ivan.grginov@infinum.hr>
2020-02-29 16:25:01 +0100

    Add parent wrapper attribute.

--- src/helpers/class-components.php
+++ src/helpers/class-components.php
@@ -48,6 +48,10 @@ class Components {
 
   /**
    * Renders a components and (optionally) passes some attributes to it.
+   * 
+   * Note about "parentClass" attribute: If provided, the component will be wrapped with a 
+   * parent BEM selector. For example, if $attributes['parentClass'] === 'header' and $component === 'logo'
+   * are set, the component will be wrapped with a <div class="header__logo"></div>
    *
    * @param  string $component  Component's name or full path (ending with .php).
    * @param  array  $attributes Array of attributes that's implicitly passed to component.
@@ -59,18 +63,28 @@ class Components {
 
     // Detect if user passed component name or path.
     if ( strpos( $component, '.php' ) !== false ) {
-      $component_path = $component;
+      $template = locate_template( $component );
     } else {
-      $component_path = "src/blocks/components/$component/$component.php";
+      $template = locate_template( "src/blocks/components/$component/$component.php" );
     }
-
-    $template = \locate_template( $component_path );
 
     if ( empty( $template ) ) {
-      Component_Exception::throw_unable_to_locate_component( $component_path );
+      throw new \Exception( "Unable to locate component: \"{$component}\"" );
     }
+
     ob_start();
+
+    // If attribute['parentClass'] is set, wrap component with parentClass__$component.
+    if ( isset( $attributes['parentClass'] ) ) {
+      echo wp_kses_post( "<div class=\"{$attributes['parentClass']}__{$component}\">" );
+    }
+
     require $template;
+
+    if ( isset( $attributes['parentClass'] ) ) {
+      echo '</div>';
+    }
+
     return ob_get_clean();
   }
 }
