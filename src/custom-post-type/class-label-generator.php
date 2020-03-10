9a14bb8017f07e8aeae2b6de413fb017de8d6999
Denis Žoljom <denis.zoljom@gmail.com>
2020-03-10 14:58:20 +0100

    Fix the wrong text-domain

--- src/custom-post-type/class-label-generator.php
+++ src/custom-post-type/class-label-generator.php
@@ -91,59 +91,59 @@ final class Label_Generator {
 
     $label_templates = [
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'name'                  => esc_html_x( '%3$s', 'Post Type General Name', 'developer-portal' ), /* phpcs:disable */
+      'name'                  => esc_html_x( '%3$s', 'Post Type General Name', 'eightshift-libs' ), /* phpcs:disable */
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'singular_name'         => esc_html_x( '%1$s', 'Post Type Singular Name', 'developer-portal' ), /* phpcs:disable */
+      'singular_name'         => esc_html_x( '%1$s', 'Post Type Singular Name', 'eightshift-libs' ), /* phpcs:disable */
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'menu_name'             => esc_html__( '%3$s', 'developer-portal' ), /* phpcs:disable */
+      'menu_name'             => esc_html__( '%3$s', 'eightshift-libs' ), /* phpcs:disable */
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'name_admin_bar'        => esc_html__( '%1$s', 'developer-portal' ), /* phpcs:disable */
+      'name_admin_bar'        => esc_html__( '%1$s', 'eightshift-libs' ), /* phpcs:disable */
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'archives'              => esc_html__( '%1$s Archives', 'developer-portal' ),
+      'archives'              => esc_html__( '%1$s Archives', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'attributes'            => esc_html__( '%1$s Attributes', 'developer-portal' ),
+      'attributes'            => esc_html__( '%1$s Attributes', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'parent_item_colon'     => esc_html__( 'Parent %1$s:', 'developer-portal' ),
+      'parent_item_colon'     => esc_html__( 'Parent %1$s:', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'all_items'             => esc_html__( 'All %3$s', 'developer-portal' ),
+      'all_items'             => esc_html__( 'All %3$s', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'add_new_item'          => esc_html__( 'Add New %1$s', 'developer-portal' ),
+      'add_new_item'          => esc_html__( 'Add New %1$s', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'add_new'               => esc_html__( 'Add New', 'developer-portal' ),
+      'add_new'               => esc_html__( 'Add New', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'new_item'              => esc_html__( 'New %1$s', 'developer-portal' ),
+      'new_item'              => esc_html__( 'New %1$s', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'edit_item'             => esc_html__( 'Edit %1$s', 'developer-portal' ),
+      'edit_item'             => esc_html__( 'Edit %1$s', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'update_item'           => esc_html__( 'Update %1$s', 'developer-portal' ),
+      'update_item'           => esc_html__( 'Update %1$s', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'view_item'             => esc_html__( 'View %1$s', 'developer-portal' ),
+      'view_item'             => esc_html__( 'View %1$s', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'view_items'            => esc_html__( 'View %3$s', 'developer-portal' ),
+      'view_items'            => esc_html__( 'View %3$s', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'search_items'          => esc_html__( 'Search %1$s', 'developer-portal' ),
+      'search_items'          => esc_html__( 'Search %1$s', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'not_found'             => esc_html__( 'Not found', 'developer-portal' ),
+      'not_found'             => esc_html__( 'Not found', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'not_found_in_trash'    => esc_html__( 'Not found in Trash', 'developer-portal' ),
+      'not_found_in_trash'    => esc_html__( 'Not found in Trash', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'featured_image'        => esc_html__( 'Featured Image', 'developer-portal' ),
+      'featured_image'        => esc_html__( 'Featured Image', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'set_featured_image'    => esc_html__( 'Set featured image', 'developer-portal' ),
+      'set_featured_image'    => esc_html__( 'Set featured image', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'remove_featured_image' => esc_html__( 'Remove featured image', 'developer-portal' ),
+      'remove_featured_image' => esc_html__( 'Remove featured image', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'use_featured_image'    => esc_html__( 'Use as featured image', 'developer-portal' ),
+      'use_featured_image'    => esc_html__( 'Use as featured image', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'insert_into_item'      => esc_html__( 'Insert into %2$s', 'developer-portal' ),
+      'insert_into_item'      => esc_html__( 'Insert into %2$s', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'uploaded_to_this_item' => esc_html__( 'Uploaded to this %2$s', 'developer-portal' ),
+      'uploaded_to_this_item' => esc_html__( 'Uploaded to this %2$s', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'items_list'            => esc_html__( '%3$s list', 'developer-portal' ),
+      'items_list'            => esc_html__( '%3$s list', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'items_list_navigation' => esc_html__( '%3$s list navigation', 'developer-portal' ),
+      'items_list_navigation' => esc_html__( '%3$s list navigation', 'eightshift-libs' ),
       /* Translators: %1$s uc singular, %2$s lc singular, %3$s uc plural, %4$s lc plural. */
-      'filter_items_list'     => esc_html__( 'Filter %4$s list', 'developer-portal' ),
+      'filter_items_list'     => esc_html__( 'Filter %4$s list', 'eightshift-libs' ),
     ];
 
     return array_map(
