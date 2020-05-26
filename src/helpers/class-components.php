18001d503a918b5353e36edf83fcbba5de7d2770
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-05-26 22:36:19 +0200

    helper fix

--- src/helpers/class-components.php
+++ src/helpers/class-components.php
@@ -110,7 +110,7 @@ class Components {
     $output = [];
 
     foreach ( $items as $item_key => $item_value ) {
-      if ( empty( $item_value ) || $item_value === false ) {
+      if ( empty( $item_value ) && $item_value !== 0 ) {
         continue;
       }
 
