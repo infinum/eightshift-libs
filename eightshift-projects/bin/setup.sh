bc06343a0afae3545c4bc60a6dbffd187b8c548e
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-07-09 16:20:26 +0200

    adding setup script for deployment

---
+++ eightshift-projects/bin/setup.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env sh
+
+set -e
+
+setupFile='setup.json';
+
+# Check if dump exists
+if [ ! -f $setupFile ]; then
+  echo "${RED}Fail! File $setupFile doesn't exist i the root of your project!${NC}"
+  exit 1
+fi
+
+# Check if core key exists in config.
+core=$(jq "select(.core) | .core" $setupFile);
+
+# Install core version.
+if [[ ! -z "$core" ]]; then
+  wp core update --version=$core --force
+
+  echo "-------------------------------------"
+fi
+
+# Check if plugins key exists in config.
+plugins=$(jq "select(.plugins) | .plugins" $setupFile);
+
+if [[ ! -z "$plugins" ]]; then
+
+  # Check if plugins core key exists in config.
+  pluginsCore=$(jq "select($plugins.core) | $plugins.core" $setupFile);
+
+  # Instale core plugins.
+  for k in $(jq "$pluginsCore | keys | .[]" $setupFile); do
+      name=$(jq -r "$k" $setupFile);
+      version=$(jq -r "$pluginsCore[$k]" $setupFile);
+
+      wp plugin install $name --version=$version --force;
+      echo "-------------------------------------"
+  done
+
+  # Check if plugins github key exists in config.
+  pluginsGithub=$(jq "select($plugins.github) | $plugins.github" $setupFile);
+
+  # Instale github plugins.
+  for k in $(jq "$pluginsGithub | keys | .[]" $setupFile); do
+      name=$(jq -r "$k" $setupFile);
+      version=$(jq -r "$pluginsGithub[$k]" $setupFile);
+
+      wp plugin install https://github.com/$name/archive/$version.zip --force;
+      echo "-------------------------------------"
+  done
+fi
