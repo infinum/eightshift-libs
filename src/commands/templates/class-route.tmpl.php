10281117088baecb9cbcb04aaf2bd0217e1c42a7
Denis Žoljom <denis.zoljom@gmail.com>
2020-07-13 21:42:48 +0200

    Add a template placeholder file

---
+++ src/commands/templates/class-route.tmpl.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * The class register route for $class_name endpoint
+ *
+ * @package Eightshift_Boilerplate\Rest
+ */
+
+namespace Eightshift_Boilerplate\Rest;
+
+use Eightshift_Libs\Core\Config_Data;
+
+/**
+ * Class Example_Route
+ */
+class %CLASS_NAME% extends Base_Route implements Callable_Route {
+
+  /**
+   * Route slug
+   *
+   * @var string
+   */
+  const ENDPOINT_SLUG = '/%ENDPOINT%';
+
+  /**
+   * Instance variable of project config data.
+   *
+   * @var object
+   */
+  protected $config;
+
+  /**
+   * Create a new instance that injects classes
+   *
+   * @param Config_Data $config Inject config which holds data regarding project details.
+   */
+  public function __construct( Config_Data $config ) {
+    $this->config = $config;
+  }
+
+  /**
+   * Method that returns project Route namespace.
+   *
+   * @return string Project namespace for REST route.
+   */
+  protected function get_namespace(): string {
+    return $this->config->get_project_routes_namespace();
+  }
+
+  /**
+   * Method that returns project route version.
+   *
+   * @return string Route version as a string.
+   */
+  protected function get_version(): string {
+    return $this->config->get_project_routes_version();
+  }
+
+  /**
+   * Get the base url of the route
+   *
+   * @return string The base URL for route you are adding.
+   */
+  protected function get_route_name(): string {
+    return static::ENDPOINT_SLUG;
+  }
+
+  /**
+   * Get callback arguments array
+   *
+   * @return array Either an array of options for the endpoint, or an array of arrays for multiple methods.
+   */
+  protected function get_callback_arguments(): array {
+    return [
+      'methods'  => static::%VERB%,
+      'callback' => [ $this, 'route_callback' ],
+    ];
+  }
+
+  /**
+   * Method that returns rest response
+   *
+   * @param \WP_REST_Request $request Data got from endpoint url.
+   *
+   * @return WP_REST_Response|mixed If response generated an error, WP_Error, if response
+   *                                is already an instance, WP_HTTP_Response, otherwise
+   *                                returns a new WP_REST_Response instance.
+   */
+  public function route_callback( \WP_REST_Request $request ) {
+    return \rest_ensure_response();
+  }
+}
