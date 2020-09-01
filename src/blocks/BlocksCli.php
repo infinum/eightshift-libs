5af53d5d3f484b329e8c923ea64e0247be3db6d2
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-09-02 01:44:14 +0200

    update

--- src/blocks/BlocksCli.php
+++ src/blocks/BlocksCli.php
@@ -19,6 +19,37 @@ class BlocksCli extends AbstractCli {
    */
   const OUTPUT_DIR = 'src/blocks';
 
+  const COMPONENTS = [
+    'button',
+    'heading',
+    'image',
+    'link',
+    'lists',
+    'paragraph',
+    'tracking',
+    'video',
+    'google-rich-snippets',
+    'header',
+    'footer',
+    'logo',
+    'drawer',
+    'menu',
+    'hamburger',
+    'copyright',
+    'page-overlay',
+  ];
+  
+  const BLOCKS = [
+    'button',
+    'heading',
+    'image',
+    'link',
+    'lists',
+    'paragraph',
+    'video',
+    'example',
+  ];
+
   /**
    * Get WPCLI command doc.
    *
@@ -44,5 +75,45 @@ class BlocksCli extends AbstractCli {
 
     // Output final class to new file/folder and finish.
     $this->output_write( static::OUTPUT_DIR, $class_name, $class );
+
+    if ( function_exists( 'add_action' ) ) {
+      $this->blocks_init();
+    }
+  }
+
+  /**
+   * Copy blocks from Eightshift-frontend-libs to project.
+   *
+   * @param bool $all Copy all from Eightshift-frontend-libs to project or selective from the list.
+   *
+   * @return void
+   */
+  public function blocks_init( bool $all = false ) : void {
+    $root = dirname( __DIR__, 5 );
+    $rootNode = "{$root}/node_modules/@eightshift/frontend-libs/blocks/init";
+
+    system( "cp -R {$rootNode}/assets {$root}/assets");
+    system( "cp -R {$rootNode}/storybook {$root}/.storybook");
+
+    if ( $all ) {
+      system( "cp -R {$rootNode}/src/blocks {$root}/src/blocks");
+    } else {
+      system( "cp -R {$rootNode}/src/blocks/assets {$root}/src/blocks/assets/");
+      system( "cp -R {$rootNode}/src/blocks/variations {$root}/src/blocks/variations/");
+      system( "cp -R {$rootNode}/src/blocks/wrapper {$root}/src/blocks/wrapper/");
+      system( "cp -R {$rootNode}/src/blocks/manifest.json {$root}/src/blocks/");
+
+      foreach( static::COMPONENTS as $component ) {
+        system( "mkdir -p {$root}/src/blocks/components/{$component}/");
+        system( "cp -R {$rootNode}/src/blocks/components/{$component}/. {$root}/src/blocks/components/{$component}/");
+      }
+
+      foreach( static::BLOCKS as $block ) {
+        system( "mkdir -p {$root}/src/blocks/custom/{$block}/");
+        system( "cp -R {$rootNode}/src/blocks/custom/{$block}/. {$root}/src/blocks/custom/{$block}/");
+      }
+    }
+
+    \WP_CLI::success( 'Blocks successfully set.' );
   }
 }
