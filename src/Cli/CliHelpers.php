778af8d1959fb57af11ac5cf78c16c14baf292c4
Denis Žoljom <denis.zoljom@gmail.com>
2020-12-01 18:23:52 +0100

    Remove methods to parent class

---
+++ src/Cli/CliHelpers.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * WPCLI Helper methods.
+ *
+ * @package EightshiftLibs\Cli
+ */
+
+declare(strict_types=1);
+
+namespace EightshiftLibs\Cli;
+
+use WP_CLI\ExitException;
+
+/**
+ * CliHelpers trait
+ */
+trait CliHelpers
+{
+	/**
+	 * WP CLI error logging helper
+	 *
+	 * A wrapper for the WP_CLI::error with error handling.
+	 *
+	 * @param string $errorMessage Error message to log in the CLI.
+	 *
+	 * @return void
+	 */
+	public static function cliError(string $errorMessage): void
+	{
+		try {
+			\WP_CLI::error($errorMessage);
+		} catch (ExitException $e) {
+			exit("{$e->getCode()}: {$e->getMessage()}");
+		}
+	}
+}
