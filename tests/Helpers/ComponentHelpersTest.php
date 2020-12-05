c6486b85d3ab8c3e11bf6cbee7bef3d7dc870e44
Denis Žoljom <denis.zoljom@gmail.com>
2020-12-05 12:15:48 +0100

    Update tests and datasets
    Arrays need to be wrapped twice in a dataset, otherwise they are interpreted as string arguments.

--- tests/Helpers/ComponentHelpersTest.php
+++ tests/Helpers/ComponentHelpersTest.php
@@ -8,7 +8,7 @@ test('Asserts ensure string returns a correct result', function ($args) {
 	$this->assertIsString(Components::ensureString($args));
 })->with('correctArguments');
 
-test('Throws type exception if wrong argument type is passed',
+test('Throws type exception if wrong argument type is passed to ensureString',
 	function ($argument) {
 		Components::ensureString($argument);
 	})
@@ -18,3 +18,15 @@ test('Throws type exception if wrong argument type is passed',
 test('Throws argument count exception if no argument is passed', function () {
 	Components::ensureString();
 })->throws(\ArgumentCountError::class);
+
+test('Asserts classnames returns a string', function ($args) {
+	error_log(print_r(Components::classnames($args), true));
+	$this->assertIsString(Components::classnames($args));
+})->with('classesArray');
+
+test('Throws type exception if wrong argument type is passed to classnames',
+	function ($argument) {
+		Components::classnames($argument);
+	})
+	->throws(\TypeError::class)
+	->with('errorStringArguments');
