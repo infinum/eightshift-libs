5a2e927c8bac0ee9c2ea2d8e0c0a67c32ca7e271
Denis Žoljom <denis.zoljom@gmail.com>
2020-12-05 14:29:32 +0100

    Update tests
    Add fake component template, added some Brain Monkey stubs

--- tests/Helpers.php
+++ tests/Helpers.php
@@ -2,4 +2,10 @@
 
 namespace Tests;
 
-// ..
+use Brain\Monkey\Functions;
+
+// Mock the template dir location.
+Functions\when('get_template_directory')->justReturn(dirname(__FILE__, 2) . '/data');
+
+// Mock escaping function.
+Functions\when('wp_kses_post')->returnArg();
