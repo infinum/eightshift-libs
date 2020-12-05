4eba7ad0da20953f1f32cc45fab37a0172575635
Denis Žoljom <denis.zoljom@gmail.com>
2020-12-05 14:31:49 +0100

    Add Brain Monkey stubs

--- tests/Helpers.php
+++ tests/Helpers.php
@@ -4,8 +4,13 @@ namespace Tests;
 
 use Brain\Monkey\Functions;
 
+// Mock WP functions
+Functions\stubTranslationFunctions();
+Functions\stubEscapeFunctions();
+
 // Mock the template dir location.
 Functions\when('get_template_directory')->justReturn(dirname(__FILE__, 2) . '/data');
 
 // Mock escaping function.
 Functions\when('wp_kses_post')->returnArg();
+
