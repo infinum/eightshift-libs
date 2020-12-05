225e29652fecd8f2e8e4ff92fd4c87ee9cfaefac
Denis Žoljom <denis.zoljom@gmail.com>
2020-12-05 17:40:45 +0100

    Add trim around selectors to remove empty string situations in them

--- src/Helpers/Components.php
+++ src/Helpers/Components.php
@@ -260,11 +260,15 @@ class Components
 		$fullModifier = '';
 		$fullElement = '';
 
-		if ($element) {
+		$element = trim($element);
+		$modifier = trim($modifier);
+		$block = trim($block);
+
+		if (!empty($element)) {
 			$fullElement = "__{$element}";
 		}
 
-		if ($modifier) {
+		if (!empty($modifier)) {
 			$fullModifier = "--{$modifier}";
 		}
 
