f7e924688a51c62bcca023f7d32fa868400a5fe4
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-01-06 00:46:03 +0100

    updating docs

--- src/Cli/CliInitProject.php
+++ src/Cli/CliInitProject.php
@@ -14,6 +14,7 @@ use EightshiftLibs\Blocks\BlocksCli;
 use EightshiftLibs\Build\BuildCli;
 use EightshiftLibs\CiExclude\CiExcludeCli;
 use EightshiftLibs\Config\ConfigCli;
+use EightshiftLibs\ConfigProject\ConfigProjectCli;
 use EightshiftLibs\Enqueue\Admin\EnqueueAdminCli;
 use EightshiftLibs\Enqueue\Blocks\EnqueueBlocksCli;
 use EightshiftLibs\Enqueue\Theme\EnqueueThemeCli;
@@ -47,6 +48,7 @@ class CliInitProject extends AbstractCli
 		SetupCli::class,
 		CiExcludeCli::class,
 		BuildCli::class,
+		ConfigProjectCli::class,
 	];
 
 	/**
