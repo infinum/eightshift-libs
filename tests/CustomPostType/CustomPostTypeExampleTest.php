3ef4a73cc8805c62bdd53656e08f8bd77101ab40
Denis Žoljom <denis.zoljom@gmail.com>
2021-01-16 18:39:25 +0100

    Separate hook tests in the separate test

---
+++ tests/CustomPostType/CustomPostTypeExampleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit\CustomPostType;
+
+use Brain\Monkey;
+use EightshiftBoilerplate\CustomPostType\PostTypeExample;
+
+/**
+ * Mock before tests.
+ */
+beforeEach(function () {
+	Monkey\setUp();
+});
+
+/**
+ * Cleanup after tests.
+ */
+afterEach(function () {
+	Monkey\tearDown();
+});
+
+
+test('Register method will call init hook', function () {
+	(new PostTypeExample())->register();
+
+	$this->assertSame(10, has_action('init', 'EightshiftBoilerplate\CustomPostType\PostTypeExample->postTypeRegisterCallback()'));
+});
