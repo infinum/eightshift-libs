764faf411809b87fe5ad8654bb7169c689ad2e81
Denis Žoljom <denis.zoljom@gmail.com>
2021-01-18 09:27:42 +0100

    Update menu tests

--- tests/Menu/MenuExampleTest.php
+++ tests/Menu/MenuExampleTest.php
@@ -8,14 +8,14 @@ use EightshiftBoilerplate\Menu\MenuExample;
 /**
  * Mock before tests.
  */
-beforeEach(function () {
+beforeAll(function () {
 	Monkey\setUp();
 });
 
 /**
  * Cleanup after tests.
  */
-afterEach(function () {
+afterAll(function () {
 	Monkey\tearDown();
 });
 
@@ -24,4 +24,12 @@ test('Register method will call init hook', function () {
 	(new MenuExample())->register();
 
 	$this->assertSame(11, has_action('after_setup_theme', 'EightshiftBoilerplate\Menu\MenuExample->registerMenuPositions()'));
+});
+
+
+test('Menu example contains correct menu position', function () {
+	$menuPositions = (new MenuExample())->getMenuPositions();
+
+	$this->assertIsArray($menuPositions, 'Menu positions should be an array');
+	$this->assertArrayHasKey('header_main_nav', $menuPositions);
 });
