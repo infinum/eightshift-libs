8c1f719dc7364d4c6dae1bf6e94bfe40012f0268
Denis Žoljom <denis.zoljom@gmail.com>
2021-02-28 11:45:20 +0100

    Add walker as a dependency, remove unused parameters in the bem menu

--- src/Menu/AbstractMenu.php
+++ src/Menu/AbstractMenu.php
@@ -51,24 +51,25 @@ abstract class AbstractMenu implements ServiceInterface, MenuPositionsInterface
 	/**
 	 * This method returns an instance of the bemMenuWalker class with the following arguments
 	 *
-	 * @param string $name Name of the menu. If theme location is provided and set
-	 *                     it will render the menu from the theme location.
-	 *                     If not it will render the name of the menu.
-	 * @param string $cssClassPrefix This string will prefix all of the menu's classes, BEM syntax friendly.
-	 * @param string $parentClass This string will add class to only top level list element.
-	 * @param string $jsModifier This string will add class to only top level list element.
-	 * @param string $cssClassModifiers Provide either a string or array of values to apply extra classes
-	 *                                   to the <ul> but not the <li's>.
-	 * @param bool   $echo Echo the menu.
+	 * @param string       $name Name of the menu. If theme location is provided and set
+	 *                           it will render the menu from the theme location.
+	 *                           If not it will render the name of the menu.
+	 * @param string       $cssClassPrefix This string will prefix all of the menu's classes, BEM syntax friendly.
+	 * @param string       $jsModifier This string will add class to only top level list element.
+	 * @param array|string $cssClassModifiers Provide either a string or array of values to apply extra classes
+	 *                                       to the <ul> but not the <li's>.
+	 * @param object|null  $walker Walker menu instance.
+	 *                             For best results use BemMenuWalker($cssClassPrefix, $parentClass).
+	 * @param bool         $echo Echo the menu.
 	 *
 	 * @return string|false|void Menu output if $echo is false, false if there are no items or no menu was found.
 	 */
 	public static function bemMenu(
 		string $name = self::MAIN_MENU,
 		string $cssClassPrefix = 'main-menu',
-		string $parentClass = '',
 		string $jsModifier = '',
 		$cssClassModifiers = '',
+		object $walker = null,
 		bool $echo = true
 	) {
 		// Check to see if any css modifiers were supplied.
@@ -88,7 +89,7 @@ abstract class AbstractMenu implements ServiceInterface, MenuPositionsInterface
 			'container' => false,
 			'items_wrap' => '<ul class="' . trim($menuWrapperClass . ' ' . $jsModifier) . '">%3$s</ul>',
 			'echo' => $echo,
-			'walker' => new BemMenuWalker($cssClassPrefix, $parentClass),
+			'walker' => $walker,
 		];
 
 		if (\has_nav_menu($name)) {
