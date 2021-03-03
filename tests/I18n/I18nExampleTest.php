11ee8606d8acff8468e2fe49ead517a3d33f9900
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-03-03 16:25:37 +0100

    Services tests

--- tests/I18n/I18nExampleTest.php
+++ tests/I18n/I18nExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\I18n;
 
 use Brain\Monkey;
+use Brain\Monkey\Functions;
 use EightshiftBoilerplate\I18n\I18nExample;
 
 use function Tests\setupMocks;
@@ -11,16 +12,34 @@ beforeEach(function() {
 	Monkey\setUp();
 	setupMocks();
 
-	$this->example = new I18nExample();
+	$this->i18n = new I18nExample();
 });
 
 afterEach(function() {
 	Monkey\tearDown();
 });
 
-
 test('Register method will call init hook', function () {
-	$this->example->register();
+	$this->i18n->register();
 
 	$this->assertSame(20, has_action('after_setup_theme', 'EightshiftBoilerplate\I18n\I18nExample->loadThemeTextdomain()'));
+});
+
+test('Registering load_theme_textdomain works', function () {
+
+	// Set up a side-affect.
+	putenv('I18N_ABSTRACTED=false');
+
+
+	\Mockery::mock('alias:EightshiftBoilerplate\Config\Config')
+	->shouldReceive('getProjectName', 'getProjectPath')
+	->andReturn('CoolProject', 'projectPath');
+
+	Functions\when('load_theme_textdomain')->alias(function() {
+		putenv('I18N_ABSTRACTED=true');
+	});
+
+	$this->i18n->loadThemeTextdomain();
+
+	$this->assertSame(getenv('I18N_ABSTRACTED'), 'true', 'Calling void method load_theme_textdomain caused no sideaffects');
 });
