a8fd4263a8968fc91cc07aed9cc53846b44c5d7f
gabriel-glo <gabriel.glogoski@infinum.hr>
2021-03-10 09:58:21 +0100

    Add EscapedView test

---
+++ tests/View/EscapedViewCliTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Unit\Config;
+
+use EightshiftLibs\View\EscapedViewCli;
+
+use function Tests\deleteCliOutput;
+
+/**
+ * Mock before tests.
+ */
+beforeEach(function () {
+	$wpCliMock = \Mockery::mock('alias:WP_CLI');
+
+	$wpCliMock
+		->shouldReceive('success')
+		->andReturnArg(0);
+
+	$wpCliMock
+		->shouldReceive('error')
+		->andReturnArg(0);
+
+	$this->escapedView = new EscapedViewCli('boilerplate');
+});
+
+/**
+ * Cleanup after tests.
+ */
+afterEach(function () {
+	$output = dirname(__FILE__, 3) . '/cliOutput';
+
+	deleteCliOutput($output);
+});
+
+
+test('Escaped view command will correctly copy the EscapedView class with defaults', function () {
+	$escapedView = $this->escapedView;
+	$escapedView([], $escapedView->getDevelopArgs([]));
+
+	// Check the output dir if the generated method is correctly generated.
+	$generatedEscapedView = file_get_contents(dirname(__FILE__, 3) . '/cliOutput/src/View/EscapedView.php');
+
+	$this->assertStringContainsString('class EscapedView extends AbstractEscapedView implements ServiceInterface', $generatedEscapedView);
+	$this->assertStringContainsString('register', $generatedEscapedView);
+});
+
+
+// test('Custom acf meta CLI documentation is correct', function () {
+// 	$config = $this->config;
+
+// 	$documentation = $config->getDoc();
+
+// 	$descKey = 'shortdesc';
+// 	$synopsisKey = 'synopsis';
+
+// 	$this->assertIsArray($documentation);
+// 	$this->assertArrayHasKey($descKey, $documentation);
+// 	$this->assertArrayHasKey($synopsisKey, $documentation);
+// 	$this->assertIsArray($documentation[$synopsisKey]);
+// 	$this->assertEquals('Generates project config class.', $documentation[$descKey]);
+
+// 	$this->assertEquals('assoc', $documentation[$synopsisKey][0]['type']);
+// 	$this->assertEquals('name', $documentation[$synopsisKey][0]['name']);
+// 	$this->assertEquals('Define project name.', $documentation[$synopsisKey][0]['description']);
+// 	$this->assertEquals(true, $documentation[$synopsisKey][0]['optional']);
+
+// 	$this->assertEquals('assoc', $documentation[$synopsisKey][1]['type']);
+// 	$this->assertEquals('version', $documentation[$synopsisKey][1]['name']);
+// 	$this->assertEquals('Define project version.', $documentation[$synopsisKey][1]['description']);
+// 	$this->assertEquals(true, $documentation[$synopsisKey][1]['optional']);
+
+// 	$this->assertEquals('assoc', $documentation[$synopsisKey][2]['type']);
+// 	$this->assertEquals('routes_version', $documentation[$synopsisKey][2]['name']);
+// 	$this->assertEquals('Define project REST version.', $documentation[$synopsisKey][2]['description']);
+// 	$this->assertEquals(true, $documentation[$synopsisKey][2]['optional']);
+// });
