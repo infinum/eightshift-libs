51d7d51edc00a35500dcddb69d01d54923e11302
Kristijan Maric <kristijan.maric@infinum.com>
2021-03-16 16:05:34 +0100

    add abstract class for block pattern registration

---
+++ src/BlockPattern/AbstractBlockPattern.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * File that holds base abstract class for block pattern.
+ *
+ * @package EightshiftLibs\BlockPattern
+ */
+
+declare(strict_types=1);
+
+namespace EightshiftLibs\BlockPattern;
+
+use EightshiftLibs\Services\ServiceInterface;
+
+/**
+ * Abstract class AbstractBlockPattern class.
+ */
+abstract class AbstractBlockPattern implements ServiceInterface
+{
+
+	/**
+	 * Register block pattern.
+	 *
+	 * @return void
+	 */
+	public function register(): void
+	{
+		\add_action('init', [$this, 'registerBlockPattern']);
+	}
+
+	/**
+	 * Method that registers new block pattern that is used inside init hook.
+	 *
+	 * @return void
+	 */
+	public function registerBlockPattern(): void
+	{
+		if ( class_exists( 'WP_Block_Patterns_Registry' ) ) {
+			\register_block_pattern(
+				$this->getName(),
+				[
+					'title' => $this->getTitle(),
+					'description' => $this->getDescription(),
+					'content' => $this->getContent(),
+					'categories' => $this->getCategories(),
+					'keywords' => $this->getKeywords(),
+				],
+			);
+		}
+	}
+
+	/**
+	 * Get the pattern categories.
+	 *
+	 * @return array Array of categories.
+	 */
+	protected function getCategories() : array {
+		return [];
+	}
+
+	/**
+	 * Get the pattern keywords.
+	 *
+	 * @return array Array of keywords.
+	 */
+	protected function getKeywords() : array {
+		return [];
+	}
+
+	/**
+	 * Get the pattern name with namespace.
+	 * Example: eightshift/pattern-name
+	 *
+	 * @return string Pattern name.
+	 */
+	abstract protected function getName(): string;
+
+	/**
+	 * Get the pattern title that is shown in the pattern selector.
+	 *
+	 * @return string Pattern title.
+	 */
+	abstract protected function getTitle(): string;
+
+	/**
+	 * Get the pattern description.
+	 *
+	 * @return string Pattern description.
+	 */
+	abstract protected function getDescription(): string;
+
+	/**
+	 * Get the pattern content.
+	 * NOTE: While returning the pattern content set it inside double quotes ("")
+	 * to avoid formatting issues.
+	 *
+	 * @return string Pattern content.
+	 */
+	abstract protected function getContent(): string;
+}
