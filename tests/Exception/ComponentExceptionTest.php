3f46c97620c617e51614d6d4b393b3e631fbf942
Igor Obradović <igor.obradovic@infinum.com>
2021-03-24 09:03:26 +0100

    Add tests for exception if not string or array

--- tests/Exception/ComponentExceptionTest.php
+++ tests/Exception/ComponentExceptionTest.php
@@ -21,17 +21,29 @@ afterEach(function() {
 test('Throws exception if ensure_string argument is invalid.', function () {
 
 	$object = new stdClass();
+	$integer = 7;
+	$null = null;
+	$bool = true;
 
 	$exceptionObject = ComponentException::throwNotStringOrArray($object);
-	$exceptionInteger = ComponentException::throwNotStringOrArray(7);
-	$exceptionNull = ComponentException::throwNotStringOrArray(null);
-	$exceptionBool = ComponentException::throwNotStringOrArray(true);
+	$exceptionInteger = ComponentException::throwNotStringOrArray($integer);
+	$exceptionNull = ComponentException::throwNotStringOrArray($null);
+	$exceptionBool = ComponentException::throwNotStringOrArray($bool);
+
+	$this->assertIsObject($exceptionObject);
+	$this->assertIsObject($exceptionInteger);
+	$this->assertIsObject($exceptionNull);
+	$this->assertIsObject($exceptionBool);
 	
 	$this->assertObjectHasAttribute('message', $exceptionObject);
 	$this->assertObjectHasAttribute('message', $exceptionInteger);
 	$this->assertObjectHasAttribute('message', $exceptionNull);
 	$this->assertObjectHasAttribute('message', $exceptionBool);
-	// $this->assertAttributeEquals('Object couldn\'t be converted to string. Please provide only string or array.', 'message', $exception);
+
+	$this->assertStringContainsString('Object couldn&#039;t be converted to string. Please provide only string or array.', $exceptionObject->getMessage());
+	$this->assertStringContainsString("{$integer} variable is not a string or array but rather integer", $exceptionInteger->getMessage());
+	$this->assertStringContainsString("{$null} variable is not a string or array but rather NULL", $exceptionNull->getMessage());
+	$this->assertStringContainsString("{$bool} variable is not a string or array but rather boolean", $exceptionBool->getMessage());
 });
 
 test('Throws exception if unable to locate component.', function () {
@@ -39,5 +51,5 @@ test('Throws exception if unable to locate component.', function () {
 	$component = 'nonexistent';
 	$output = ComponentException::throwUnableToLocateComponent($component);
 
-	$this->assertStringContainsString("Unable to locate component by path: {$component}", $output);
+	$this->assertStringContainsString("Unable to locate component by path: {$component}", $output->getMessage());
 });
