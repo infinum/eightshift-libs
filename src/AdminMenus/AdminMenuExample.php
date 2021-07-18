360f5709cd0268b16e09cff4a8479ea2ba4d87f1
Denis Žoljom <denis.zoljom@gmail.com>
2021-07-18 14:55:15 +0200

    Add admin menus templates with example
    To do: add CLI command to generate menu and submenu.

---
+++ src/AdminMenus/AdminMenuExample.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * File that holds class for admin menu example.
+ *
+ * @package EightshiftLibs\AdminMenus
+ */
+
+declare(strict_types=1);
+
+namespace EightshiftBoilerplate\AdminMenus;
+
+use EightshiftLibs\AdminMenus\AbstractAdminMenu;
+
+/**
+ * BlockPatternExample class.
+ */
+class AdminMenuExample extends AbstractAdminMenu
+{
+	/**
+	 * Capability for this post type
+	 *
+	 * @var string
+	 */
+	public const ADMIN_MENU_CAPABILITY = 'edit_posts';
+
+	/**
+	 * Get the title to use for the admin page.
+	 *
+	 * @return string The text to be displayed in the title tags of the page when the menu is selected.
+	 */
+	protected function getTitle(): string
+	{
+		return \esc_html__('Admin Title', 'eightshift-libs');
+	}
+
+	/**
+	 * Get the menu title to use for the admin menu.
+	 *
+	 * @return string The text to be used for the menu.
+	 */
+	protected function getMenuTitle(): string
+	{
+		return \esc_html__('Admin Title', 'eightshift-libs');
+	}
+
+	/**
+	 * Get the capability required for this menu to be displayed.
+	 *
+	 * @return string The capability required for this menu to be displayed to the user.
+	 */
+	protected function getCapability(): string
+	{
+		return \esc_html__('Admin Title', 'eightshift-libs');
+	}
+
+	/**
+	 * Get the menu slug.
+	 *
+	 * @return string The slug name to refer to this menu by.
+	 *                Should be unique for this menu page and only include lowercase alphanumeric,
+	 *                dashes, and underscores characters to be compatible with sanitize_key().
+	 */
+	protected function getMenuSlug(): string
+	{
+		return self::ADMIN_MENU_CAPABILITY;
+	}
+
+	/**
+	 * Get the view component that will render correct view.
+	 *
+	 * @return string View uri.
+	 */
+	protected function getViewComponent(): string
+	{
+		return 'layout';
+	}
+
+	/**
+	 * Process the admin menu attributes.
+	 *
+	 * Here you can get any kind of metadata, query the database, etc..
+	 * This data will be passed to the component view to be rendered out in the
+	 * processAdminMenu parent method.
+	 *
+	 * @param array<string, mixed>|string $attr Raw admin menu attributes passed into the
+	 *                           admin menu function.
+	 *
+	 * @return array<string, mixed> Processed admin menu attributes.
+	 */
+	protected function processAttributes($attr): array
+	{
+		return [
+			'pageTitle' => \esc_html__('Admin Title', 'eightshift-libs'),
+		];
+	}
+}
