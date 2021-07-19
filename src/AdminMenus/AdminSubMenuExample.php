e66d7c060a521c9acde299c28a1f1404578ccfcf
Denis Žoljom <denis.zoljom@gmail.com>
2021-07-19 17:55:31 +0200

    Add submenu CLI and submenu example

---
+++ src/AdminMenus/AdminSubMenuExample.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * File that holds class for admin sub menu example.
+ *
+ * @package EightshiftLibs\AdminMenus
+ */
+
+declare(strict_types=1);
+
+namespace EightshiftBoilerplate\AdminMenus;
+
+use EightshiftLibs\AdminMenus\AbstractAdminSubMenu;
+
+/**
+ * AdminSubMenuExample class.
+ */
+class AdminSubMenuExample extends AbstractAdminSubMenu
+{
+	/**
+	 * Capability for this admin sub menu
+	 *
+	 * @var string
+	 */
+	public const ADMIN_MENU_CAPABILITY = 'edit_posts';
+
+	/**
+	 * Menu slug for this admin sub menu
+	 *
+	 * @var string
+	 */
+	public const ADMIN_MENU_SLUG = 'example-menu-slug';
+
+	/**
+	 * Parent menu slug for this admin sub menu
+	 *
+	 * @var string
+	 */
+	public const PARENT_MENU_SLUG = 'example-parent-slug';
+
+	/**
+	 * Get the title to use for the admin page.
+	 *
+	 * @return string The text to be displayed in the title tags of the page when the menu is selected.
+	 */
+	protected function getTitle(): string
+	{
+		return \esc_html__('Admin Title', 'eightshift-libs');
+	}
+
+	/**
+	 * Get the menu title to use for the admin menu.
+	 *
+	 * @return string The text to be used for the menu.
+	 */
+	protected function getMenuTitle(): string
+	{
+		return \esc_html__('Admin Sub Menu Title', 'eightshift-libs');
+	}
+
+	/**
+	 * Get the capability required for this menu to be displayed.
+	 *
+	 * @return string The capability required for this menu to be displayed to the user.
+	 */
+	protected function getCapability(): string
+	{
+		return self::ADMIN_MENU_CAPABILITY;
+	}
+
+	/**
+	 * Get the menu slug.
+	 *
+	 * @return string The slug name to refer to this menu by.
+	 *                Should be unique for this menu page and only include lowercase alphanumeric,
+	 *                dashes, and underscores characters to be compatible with sanitize_key().
+	 */
+	protected function getMenuSlug(): string
+	{
+		return self::ADMIN_MENU_SLUG;
+	}
+
+	/**
+	 * Get the slug of the parent menu.
+	 *
+	 * @return string The slug name for the parent menu (or the file name of a standard WordPress admin page.
+	 */
+	protected function getParentMenu(): string
+	{
+		return self::PARENT_MENU_SLUG;
+	}
+
+	/**
+	 * Get the view component that will render correct view.
+	 *
+	 * @return string View uri.
+	 */
+	protected function getViewComponent(): string
+	{
+		return 'layout';
+	}
+
+	/**
+	 * Process the admin menu attributes.
+	 *
+	 * Here you can get any kind of metadata, query the database, etc..
+	 * This data will be passed to the component view to be rendered out in the
+	 * processAdminMenu parent method.
+	 *
+	 * @param array<string, mixed>|string $attr Raw admin menu attributes passed into the
+	 *                           admin menu function.
+	 *
+	 * @return array<string, mixed> Processed admin menu attributes.
+	 */
+	protected function processAttributes($attr): array
+	{
+		return [
+			'pageTitle' => \esc_html__('Admin Title', 'eightshift-libs'),
+		];
+	}
+}
