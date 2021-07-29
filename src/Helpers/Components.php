f42159b517b2b3377f6a62bfc522b439132771cc
Ivan Grginov <ivan.grginov@infinum.com>
2021-07-29 10:11:15 +0200

    Better typehint

--- src/Helpers/Components.php
+++ src/Helpers/Components.php
@@ -287,7 +287,7 @@ class Components
 	 * @throws \Exception If missing responsiveAttributes or keyName in responsiveAttributes.
 	 * @throws \Exception If missing keyName in responsiveAttributes.
 	 *
-	 * @return mixed
+	 * @return array<mixed>
 	 */
 	public static function checkAttrResponsive(string $keyName, array $attributes, array $manifest): array
 	{
