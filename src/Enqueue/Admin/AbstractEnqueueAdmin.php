1b17ba98671556860d17600525770d1620723d40
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-09-20 08:56:28 +0200

    updating admin enque to not use on block editor

--- src/Enqueue/Admin/AbstractEnqueueAdmin.php
+++ src/Enqueue/Admin/AbstractEnqueueAdmin.php
@@ -34,15 +34,11 @@ abstract class AbstractEnqueueAdmin extends AbstractAssets
 	/**
 	 * Register the Stylesheets for the admin area.
 	 *
-	 * @param string $hookSuffix The current admin page.
-	 *
 	 * @return void
 	 */
-	public function enqueueStyles(string $hookSuffix): void
+	public function enqueueStyles(): void
 	{
-		$screen = \get_current_screen();
-
-		if (is_admin() && !$screen->is_block_editor) {
+		if (!$this->getConditionUse()) {
 			$handle = "{$this->getAssetsPrefix()}-styles";
 	
 			\wp_register_style(
@@ -60,26 +56,46 @@ abstract class AbstractEnqueueAdmin extends AbstractAssets
 	/**
 	 * Register the JavaScript for the admin area.
 	 *
-	 * @param string $hookSuffix The current admin page.
-	 *
 	 * @return void
 	 */
-	public function enqueueScripts(string $hookSuffix): void
+	public function enqueueScripts(): void
 	{
-		$handle = "{$this->getAssetsPrefix()}-scripts";
+		if (!$this->getConditionUse()) {
+			$handle = "{$this->getAssetsPrefix()}-scripts";
 
-		\wp_register_script(
-			$handle,
-			$this->manifest->getAssetsManifestItem(static::ADMIN_SCRIPT_URI),
-			$this->getAdminScriptDependencies(),
-			$this->getAssetsVersion(),
-			$this->scriptInFooter()
-		);
+			\wp_register_script(
+				$handle,
+				$this->manifest->getAssetsManifestItem(static::ADMIN_SCRIPT_URI),
+				$this->getAdminScriptDependencies(),
+				$this->getAssetsVersion(),
+				$this->scriptInFooter()
+			);
 
-		\wp_enqueue_script($handle);
+			\wp_enqueue_script($handle);
 
-		foreach ($this->getLocalizations() as $objectName => $dataArray) {
-			\wp_localize_script($handle, $objectName, $dataArray);
+			foreach ($this->getLocalizations() as $objectName => $dataArray) {
+				\wp_localize_script($handle, $objectName, $dataArray);
+			}
 		}
+	}
+
+	/**
+	 * Condition script usage.
+	 *
+	 * @return boolean
+	 */
+	public function getConditionUse(): bool
+	{
+		if (!is_admin()) {
+			return false;
+		}
+
+		$screen = \get_current_screen();
+
+		if ($screen->is_block_editor) {
+			return true;
+		}
+
+		return false;
 	}
 }
