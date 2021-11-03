52014fe52d6e2669354c2baee107b3565f4a8421
Denis Žoljom <denis.zoljom@gmail.com>
2021-11-03 10:20:07 +0100

    Add test for the compiled container

---
+++ tests/data/src/ThirdMockServiceClass.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * File holding ThirdMockServiceClass class
+ *
+ * @package Tests\data\src
+ */
+
+declare(strict_types=1);
+
+namespace Tests\data\src;
+
+/**
+ * ThirdMockServiceClass class
+ *
+ * @package Tests\data\src
+ */
+class ThirdMockServiceClass
+{
+	public const TEST = 'Non service class';
+}
