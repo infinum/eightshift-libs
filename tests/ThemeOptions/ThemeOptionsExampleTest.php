1488bba8ab2009784d9d77adf1bb1c6f8d0afe7e
Igor Obradović <igor.obradovic@infinum.com>
2022-01-14 14:26:15 +0100

    Add tests for ThemeOptionsExample class

--- tests/ThemeOptions/ThemeOptionsExampleTest.php
+++ tests/ThemeOptions/ThemeOptionsExampleTest.php
@@ -18,10 +18,28 @@ afterEach(function() {
 	Monkey\tearDown();
 });
 
+test('Register method will bail out if ACF is not registered/activated', function () {
+	$this->assertNull($this->example->register());
+});
+
 test('if theme options actions are registered', function () {
 	$this->example->register();
 
 	$this->assertTrue(\method_exists($this->example, 'register'));
 	$this->assertSame(10, has_action('acf/init', [$this->example, 'createThemeOptionsPage']));
 	$this->assertSame(10, has_action('acf/init', [$this->example, 'registerThemeOptions']));
+});
+
+test('Method for adding ACF Theme Options page exists', function () {
+	$this->example->createThemeOptionsPage();
+
+	$this->assertTrue(\method_exists($this->example, 'createThemeOptionsPage'));
+	$this->assertTrue(\function_exists('acf_add_options_page'));
+});
+
+test('Method for adding ACF fields to Theme Options page exists', function () {
+	$this->example->registerThemeOptions();
+
+	$this->assertTrue(\method_exists($this->example, 'registerThemeOptions'));
+	$this->assertTrue(\function_exists('acf_add_local_field_group'));
 });
