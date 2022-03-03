0f9a1c851f39249f9fd970a89a0c1037e27f5c78
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2022-03-03 10:42:02 +0100

    update test to include new variable output and add hexToRgb tests

--- tests/Helpers/ComponentHelpersTest.php
+++ tests/Helpers/ComponentHelpersTest.php
@@ -350,6 +350,7 @@ test('Asserts that outputCssVariablesGlobal returns the correct CSS variables fr
 	$this->assertStringContainsString('<style>', $output);
 	$this->assertStringContainsString(':root {', $output);
 	$this->assertStringContainsString('--global-colors-primary: #C3151B;', $output);
+	$this->assertStringContainsString('--global-colors-primary-values: 195 21 27;', $output);
 	$this->assertStringNotContainsString('--button-content:', $output);
 });
 
@@ -373,6 +374,7 @@ test('Asserts that globalInner returns the correct css variable for color', func
 
 	$this->assertIsString($output);
 	$this->assertStringContainsString('--global-colors-primary: #C3151B;', $output);
+	$this->assertStringContainsString('--global-colors-primary-values: 195 21 27;', $output);
 });
 
 test('Asserts that globalInner returns the correct css variable for gradients', function () {
@@ -1473,4 +1475,18 @@ test('Asserts props will correctly generate manual keys in camelCase', function 
 	$this->assertIsArray($output);
 	$this->assertContains('mockCardButtonContent', array_keys($output), "Manual key is not properly converted to camelCase");
 	$this->assertNotContains('mockCardbuttonContent', array_keys($output), "Manual key is not properly converted to camelCase");
+});
+
+test('Check that hexToRgb returns the correct output for a correctly formatted hex code', function () {
+	$shorthand = Components::hexToRgb('#fff');
+	$fullHex = Components::hexToRgb('#ffffff');
+
+	$this->assertEquals('255 255 255', $shorthand);
+	$this->assertEquals('255 255 255', $fullHex);
+});
+
+test('Check that hexToRgb returns the fallback for an incorrectly formatted hex code', function () {
+	$incorrectFormat = Components::hexToRgb('#0000');
+
+	$this->assertEquals('0 0 0', $incorrectFormat);
 });
