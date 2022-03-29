409eb6644b8adffcff208e98f7d3f0362e7f4847
Ivan Ružević <ivan.ruzevic@infinum.hr>
2022-03-29 11:28:36 +0200

    fixing pr issues

--- src/Enqueue/Blocks/AbstractEnqueueBlocks.php
+++ src/Enqueue/Blocks/AbstractEnqueueBlocks.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 namespace EightshiftLibs\Enqueue\Blocks;
 
 use EightshiftLibs\Enqueue\AbstractAssets;
+use EightshiftLibs\Manifest\ManifestInterface;
 
 /**
  * Enqueue_Blocks class.
