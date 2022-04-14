ab552a88bf052c8269a00043212c8e9402710a13
Denis Žoljom <denis.zoljom@gmail.com>
2022-04-14 16:37:46 +0200

    Update Pest.php file
    Manually load the testcase file for the integration tests.

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,11 @@
 <?php
 
+// Autoload the WP integration files.
+require_once dirname(__FILE__, 2) . '/wp/tests/phpunit/includes/phpunit-adapter-testcase.php';
+require_once dirname(__FILE__, 2) . '/wp/tests/phpunit/includes/abstract-testcase.php';
+require_once dirname(__FILE__, 2) . '/wp/tests/phpunit/includes/testcase.php';
+
 uses()->group('integration')->in('Integration');
 uses()->group('unit')->in('Unit');
 
-//uses(\WP_UnitTestCase::class)->in('Integration');
+uses(\WP_UnitTestCase::class)->in('Integration');
