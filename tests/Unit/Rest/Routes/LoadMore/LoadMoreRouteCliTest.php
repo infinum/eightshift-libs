ee8fb97fe51608073b44401e2be95477fb2f7dcc
Denis Žoljom <denis.zoljom@gmail.com>
2022-07-18 19:23:57 +0200

    Add test for load more route
    Increasing the code coverage

---
+++ tests/Unit/Rest/Routes/LoadMore/LoadMoreRouteCliTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Unit\Rest\Routes\LoadMore;
+
+use EightshiftLibs\Helpers\Components;
+use EightshiftLibs\Rest\Routes\LoadMore\LoadMoreRouteCli;
+
+beforeEach(function () {
+	$this->mock = new LoadMoreRouteCli('boilerplate');
+});
+
+afterEach(function () {
+	unset($this->mock);
+});
+
+
+test('REST load more route CLI command will correctly copy the field class with defaults', function () {
+	$mock = $this->mock;
+	$mock([], $mock->getDefaultArgs());
+
+	$sep = \DIRECTORY_SEPARATOR;
+	$output = \file_get_contents(Components::getProjectPaths('cliOutput', "src{$sep}Rest{$sep}Routes{$sep}LoadMoreRoute.php"));
+
+	expect($output)
+		->toContain('class LoadMoreRoute extends AbstractRoute implements CallableRouteInterface')
+		->toContain("'methods'")
+		->toContain("'callback'")
+		->toContain("'permission_callback'")
+		->toContain('public function getMappedData')
+		->toContain('function routeCallback');
+});
+
+test('REST load more route CLI command returns correct command parent name', function () {
+	expect($this->mock->getCommandParentName())->toBe('create');
+});
+
+test('REST load more route CLI command returns correct command name', function () {
+	expect($this->mock->getCommandName())->toBe('rest-route-load-more');
+});
+
+test('REST load more route CLI command has correct documentation', function () {
+	expect($this->mock->getDoc())
+		->toBeArray()
+		->toHaveKey('shortdesc')
+		->toHaveKey('longdesc')
+		->and($this->mock->getDoc()['shortdesc'])->toBe('Create REST-API load more route class.');
+});
+
