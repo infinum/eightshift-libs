e3263780737c6368d2f8feb6beb6d8fb14b006ef
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-05-22 11:32:24 +0200

    add multiline test output

--- src/Init/InitHeaderFooter.php
+++ src/Init/InitHeaderFooter.php
@@ -89,6 +89,9 @@ class InitHeaderFooter extends AbstractCli
 
 		if (!$groupOutput) {
 			$this->cliLogAlert('Header and footer reusable blocks added');
+
+			$this->cliLogAlert('All the files have been copied, you can start working on your awesome theme!\nType `npm start` to build all the assets.', 'success', __('Ready to go!', 'eightshift-libs'));
+			$this->cliLogAlert('If you want to set up the default header and footer, run `wp boilerplate init header-footer`.', 'info', __('Note', 'eightshift-libs'));
 		}
 	}
 }
