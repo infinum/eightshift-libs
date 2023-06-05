e80855d14b73e475604c27e8b070af6277875406
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-06-05 15:52:37 +0200

    test new color

--- src/Blocks/AbstractBlocksCli.php
+++ src/Blocks/AbstractBlocksCli.php
@@ -169,7 +169,7 @@ abstract class AbstractBlocksCli extends AbstractCli
 					$this->cliLog("%g│ %n{$msgTitle} %w({$path})%n", 'mixed');
 				// } else {
 					$this->cliLogAlert(implode("\n", [
-						"%K{$path}%n",
+						"%8{$path}%n",
 						'',
 						'Run %Unpm start%n to make sure everything works correctly.'
 					]), 'success', "{$itemName} ${type} added");
