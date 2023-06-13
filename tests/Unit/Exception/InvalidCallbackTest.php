2d70b072758912c861a3c59bd4d0c4ce80369bf2
Viktor Szépe <viktor@szepe.net>
2023-06-13 07:20:08 +0000

    Fix deprecated PHPUnit method

--- tests/Unit/Exception/InvalidCallbackTest.php
+++ tests/Unit/Exception/InvalidCallbackTest.php
@@ -11,6 +11,6 @@ test('Checks if the fromCallback method will return correct response.', function
 	$exceptionObject = InvalidCallback::fromCallback($callback);
 
 	$this->assertIsObject($exceptionObject, "The {$exceptionObject} should be an instance of InvalidBlock class");
-	$this->assertObjectHasAttribute('message', $exceptionObject, "Object doesn't contain message attribute");
+	$this->assertObjectHasProperty('message', $exceptionObject, "Object doesn't contain message attribute");
 	$this->assertSame("The callback {$callback} is not recognized and cannot be registered.", $exceptionObject->getMessage(), "Strings for message if callback isn't recognised do not match!");
 });
