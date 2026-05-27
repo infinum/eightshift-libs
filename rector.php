3ec42e7c1edfa745947833909435b9ed31cd4eb3
Ivan Ružević <ivan.ruzevic@infinum.com>
2026-05-27 14:53:33 +0200

    updates

--- rector.php
+++ rector.php
@@ -14,12 +14,24 @@ use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
 return RectorConfig::configure()
-	->withPaths([__DIR__ . '/src'])
+	->withPaths([
+		__DIR__,
+	])
 	->withBootstrapFiles([__DIR__ . '/vendor/autoload.php'])
 	->withPhpSets(php84: true)
 	->withSets([LevelSetList::UP_TO_PHP_84, SetList::CODE_QUALITY, SetList::DEAD_CODE, SetList::TYPE_DECLARATION, SetList::EARLY_RETURN])
 	->withSkip([
 		RemoveUselessVarTagRector::class,
+		__DIR__ . '/vendor-prefixed/*',
+		__DIR__ . '/vendor/*',
+		__DIR__ . '/tests/*',
+		__DIR__ . '/.husky/*',
+		__DIR__ . '/.github/*',
+		__DIR__ . '/coverage/*',
+		__DIR__ . '/.phpunit.cache/*',
+		__DIR__ . '/node_modules/*',
+		__DIR__ . '/eightshift/*',
+		__DIR__ . '/public/*',
 		__DIR__ . '/src/**/*Example.php',
 	])
 	->withIndent("\t", indentSize: 1)
